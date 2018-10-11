3a1ea53db413023028006f0348788d4f929dc87b
pzy123 <18249048778@163.com>
2018-10-11 21:16:13 +0800

    10.11晚自习banner图效果结束

--- js/homepage.js
+++ js/homepage.js
@@ -39,3 +39,48 @@
         $(".tex1 dl dt span").html( $(this).html() )
     })
     //搜索部分结束
+    //导航效果开始
+    $(".long-nav1-ul .long-nav1-li").hover( function(){
+        // var index = $(this).index();
+        $(this).find(".long-nav1-li03").show().find(".long-nav1-li03").siblings().hide();
+        $(this).css( "background","#ffffff" );
+    },function(){
+        $(".long-nav1-li03").hide();
+        $(this).css( "background","" );
+    } )
+
+    $(".long-li03-div2 span").hover( function(){
+        $(this).css( "color","#cc0000" );
+    },function(){
+        $(this).css( "color","#666666" );
+    } )
+
+    $(".header3-navli").not(".long-nav").hover( function(){
+        $(this).find("a").css( "color","#cc0000" );
+    },function(){
+        $(this).find("a").css( "color","" );
+    } )
+    //导航效果结束
+    //banner开始
+    var timer = null;
+    var index = 0;
+    var list = $(".banner-dian i");
+    var img = $(".banner-img li");
+    timer = setInterval( autoplay,3000 );
+    function autoplay(){
+        index++;
+        if( index == list.size() ){
+            index = 0;
+        }
+        list.eq(index).addClass("dianred").siblings().removeClass("dianred");
+        console.log(index);
+        img.eq(index).fadeIn(2000).siblings().fadeOut(1000);
+    }
+    list.hover( function(){
+		clearInterval( timer );
+		index = $(this).index()-1;
+		autoplay();
+	},function(){
+		timer = setInterval( autoplay,2000 );
+	} )
+    //banner结束
