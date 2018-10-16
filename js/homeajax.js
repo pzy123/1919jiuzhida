e958c68bd91ee8fe3ff550cbf276ff437d53cc11
pzy123 <18249048778@163.com>
2018-10-16 12:03:23 +0800

    首页基本完事差footer最后一点点

--- js/homeajax.js
+++ js/homeajax.js
@@ -1,43 +1,252 @@
-window.onload = function(){
+//白酒专区点击请求ajax
+$(".jiangxiang").click( function(){
     $.ajax({
         type:"get",
         async:true,
         url:"./homepage.json?id="+new Date().getTime(),
         success : function(res){
-           var pro=res.list1;
-           for(var i=0;i<pro.length;i++){
-               var item=pro[i]
-           }
-    //             var pro = json[0];
-    //             cons += `
-    //             <li>
-    //                 <div class="kill-down-li-div1">
-    //                     <div class="kill-down-li-div3">
-    //                         <a href="###">
-    //                             <img src="images/xianshimiaosha (1).jpg" alt="">
-    //                         </a>
-    //                     </div>
-    //                     <div class="kill-down-li-div4">
-    //                         <span>53度1500ml天存壹号坛·红坛（献礼版）</span>
-    //                     </div>
-    //                 </div>
-    //                 <div class="kill-down-li-div2">
-    //                     <div class="kill-down-li-div5">
-    //                         <div class="kill-down-li-div7">
-    //                             <span>￥218.00</span>
-    //                             <span>￥388.00</span>
-    //                         </div>
-    //                     </div>
-    //                     <div class="kill-down-li-div6">
-    //                         <a href="###">
-    //                             <span>立即抢购</span>
-    //                         </a>
-    //                     </div>
-    //                 </div>
-    //             </li>
-    // `;
-        
-    //     $(".kill-down-leftall ul").html( cons );
+            // console.log(res);
+            
+           for( var i = 0 ; i < res.length ; i++ ){
+                var pro = res[i];
+            //    console.log(pro);
+                var pro1 = pro.classify;
+                // var pro2 = pro1[list1];
+                var pro2=pro.list1;
+                if( pro1 == "jiangxiang" ){
+                    // console.log(pro2);
+                    var cons = ""
+                    for( var j = 0 ; j < pro2.length ; j++  ){
+                        var pro3 = pro2[j]
+                        // console.log(pro3) 
+                        cons += `
+                         <li>
+                             <div class="whitecenter3-imgdiv">
+                                 <a href="###">
+                                     <img src="${pro3.src1}" alt="">
+                                 </a>
+                             </div>
+                             <div class="whitecenter3-spandiv">
+                                 <p><a href="###"><span>${pro3.span1}</span></a></p>
+                                 <p><span>${pro3.money1}</span></p>
+                                 <p><span>${pro3.money2}</span></p>
+                             </div>
+                         </li>
+                        `;    
+                }
+                    $(".whitecenter3box ul").html( cons );
+            } 
+            }   
         }
     })
-}
+} )
+
+$(".nongxiang").click( function(){
+    $.ajax({
+        type:"get",
+        async:true,
+        url:"./homepage.json?id="+new Date().getTime(),
+        success : function(res){
+            // console.log(res);
+            
+           for( var i = 0 ; i < res.length ; i++ ){
+                var pro = res[i];
+            //    console.log(pro);
+                var pro1 = pro.classify;
+                // var pro2 = pro1[list1];
+                var pro2=pro.list1;
+                if( pro1 == "nongxiang" ){
+                    // console.log(pro2);
+                    var cons = ""
+                    for( var j = 0 ; j < pro2.length ; j++  ){
+                        var pro3 = pro2[j]
+                        // console.log(pro3) 
+                        cons += `
+                         <li>
+                             <div class="whitecenter3-imgdiv">
+                                 <a href="###">
+                                     <img src="${pro3.src1}" alt="">
+                                 </a>
+                             </div>
+                             <div class="whitecenter3-spandiv">
+                                 <p><a href="###"><span>${pro3.span1}</span></a></p>
+                                 <p><span>${pro3.money1}</span></p>
+                                 <p><span>${pro3.money2}</span></p>
+                             </div>
+                         </li>
+                        `;    
+                }
+                    $(".whitecenter3box ul").html( cons );
+            } 
+            }   
+        }
+    })
+} )
+
+$(".qingxiang").click( function(){
+    $.ajax({
+        type:"get",
+        async:true,
+        url:"./homepage.json?id="+new Date().getTime(),
+        success : function(res){
+            // console.log(res);
+            
+           for( var i = 0 ; i < res.length ; i++ ){
+                var pro = res[i];
+            //    console.log(pro);
+                var pro1 = pro.classify;
+                // var pro2 = pro1[list1];
+                var pro2=pro.list1;
+                if( pro1 == "qingxiang" ){
+                    // console.log(pro2);
+                    var cons = ""
+                    for( var j = 0 ; j < pro2.length ; j++  ){
+                        var pro3 = pro2[j]
+                        // console.log(pro3) 
+                        cons += `
+                         <li>
+                             <div class="whitecenter3-imgdiv">
+                                 <a href="###">
+                                     <img src="${pro3.src1}" alt="">
+                                 </a>
+                             </div>
+                             <div class="whitecenter3-spandiv">
+                                 <p><a href="###"><span>${pro3.span1}</span></a></p>
+                                 <p><span>${pro3.money1}</span></p>
+                                 <p><span>${pro3.money2}</span></p>
+                             </div>
+                         </li>
+                        `;    
+                }
+                    $(".whitecenter3box ul").html( cons );
+            } 
+            }   
+        }
+    })
+} )
+//葡萄酒请求ajax
+$(".faguo").click( function(){
+    $.ajax({
+        type:"get",
+        async:true,
+        url:"./homepage.json?id="+new Date().getTime(),
+        success : function(res){
+            // console.log(res);
+            
+           for( var i = 0 ; i < res.length ; i++ ){
+                var pro = res[i];
+            //    console.log(pro);
+                var pro1 = pro.classify;
+                // var pro2 = pro1[list1];
+                var pro2=pro.list1;
+                if( pro1 == "faguo" ){
+                    // console.log(pro2);
+                    var cons = ""
+                    for( var j = 0 ; j < pro2.length ; j++  ){
+                        var pro3 = pro2[j]
+                        // console.log(pro3) 
+                        cons += `
+                         <li>
+                             <div class="whitecenter3-imgdiv">
+                                 <a href="###">
+                                     <img src="${pro3.src1}" alt="">
+                                 </a>
+                             </div>
+                             <div class="whitecenter3-spandiv">
+                                 <p><a href="###"><span>${pro3.span1}</span></a></p>
+                                 <p><span>${pro3.money1}</span></p>
+                                 <p><span>${pro3.money2}</span></p>
+                             </div>
+                         </li>
+                        `;    
+                }
+                    $(".putaojiu3box ul").html( cons );
+            } 
+            }   
+        }
+    })
+} )
+
+$(".aodaliya").click( function(){
+    $.ajax({
+        type:"get",
+        async:true,
+        url:"./homepage.json?id="+new Date().getTime(),
+        success : function(res){
+            // console.log(res);
+            
+           for( var i = 0 ; i < res.length ; i++ ){
+                var pro = res[i];
+            //    console.log(pro);
+                var pro1 = pro.classify;
+                // var pro2 = pro1[list1];
+                var pro2=pro.list1;
+                if( pro1 == "aodaliya" ){
+                    // console.log(pro2);
+                    var cons = ""
+                    for( var j = 0 ; j < pro2.length ; j++  ){
+                        var pro3 = pro2[j]
+                        // console.log(pro3) 
+                        cons += `
+                         <li>
+                             <div class="whitecenter3-imgdiv">
+                                 <a href="###">
+                                     <img src="${pro3.src1}" alt="">
+                                 </a>
+                             </div>
+                             <div class="whitecenter3-spandiv">
+                                 <p><a href="###"><span>${pro3.span1}</span></a></p>
+                                 <p><span>${pro3.money1}</span></p>
+                                 <p><span>${pro3.money2}</span></p>
+                             </div>
+                         </li>
+                        `;    
+                }
+                    $(".putaojiu3box ul").html( cons );
+            } 
+            }   
+        }
+    })
+} )
+
+$(".zhili").click( function(){
+    $.ajax({
+        type:"get",
+        async:true,
+        url:"./homepage.json?id="+new Date().getTime(),
+        success : function(res){
+            // console.log(res);
+            
+           for( var i = 0 ; i < res.length ; i++ ){
+                var pro = res[i];
+            //    console.log(pro);
+                var pro1 = pro.classify;
+                // var pro2 = pro1[list1];
+                var pro2=pro.list1;
+                if( pro1 == "zhili" ){
+                    // console.log(pro2);
+                    var cons = ""
+                    for( var j = 0 ; j < pro2.length ; j++  ){
+                        var pro3 = pro2[j]
+                        // console.log(pro3) 
+                        cons += `
+                         <li>
+                             <div class="whitecenter3-imgdiv">
+                                 <a href="###">
+                                     <img src="${pro3.src1}" alt="">
+                                 </a>
+                             </div>
+                             <div class="whitecenter3-spandiv">
+                                 <p><a href="###"><span>${pro3.span1}</span></a></p>
+                                 <p><span>${pro3.money1}</span></p>
+                                 <p><span>${pro3.money2}</span></p>
+                             </div>
+                         </li>
+                        `;    
+                }
+                    $(".putaojiu3box ul").html( cons );
+            } 
+            }   
+        }
+    })
+} )
